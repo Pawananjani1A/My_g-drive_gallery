8e29f6dd637a0a551cd24329726fb0776e332f20
PAWANANJANI KUMAR <pawananjanimth1@gmail.com>
2021-01-11 14:56:34 +0530

    Port fix  pushed

--- index.js
+++ index.js
@@ -544,7 +544,7 @@ if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 app.listen(
     PORT,
     console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`)
